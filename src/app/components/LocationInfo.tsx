edfb3f4e94d28af5e68050b0543dcd8601de3665
Martin Corredor <dev.martincorredor@gmail.com>
2025-03-23 18:17:38 -0500

    Add google map

--- src/app/components/LocationInfo.tsx
+++ src/app/components/LocationInfo.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image';
 import flightImage from '../assets/flight.png';
 import mapPointIcon from '../assets/icons/map_point.svg';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 interface LocationInfoProps {
   airportData: {
@@ -15,8 +16,7 @@ interface LocationInfoProps {
 const LocationInfo: React.FC<LocationInfoProps> = ({ airportData }) => {
   const lat = Number(airportData.latitude);
   const lng = Number(airportData.longitude);
-  //   const lat = -34.5956145;
-  //   const lng = -58.4431949;
+  
   const mapContainerStyle = { width: '100%', height: '300px' };
   const center = { lat, lng };
 
@@ -24,23 +24,27 @@ const LocationInfo: React.FC<LocationInfoProps> = ({ airportData }) => {
     <div className="details-section">
       <div className="details-card">
         <div className="details-card-title">
-          <Image src={mapPointIcon} alt="Avión" width={32} height={32} />
+          <Image src={mapPointIcon} alt="Ubicación" width={32} height={32} />
           <h2 className="details-card-title-label">Ubicación</h2>
         </div>
         <div className="details-card-description">
-        <p>
-        <strong>Latitud:</strong> {lat}
-      </p>
-      <p>
-        <strong>Longitud:</strong> {lng}
-      </p>
-      <p>
-        <strong>ID Geoname:</strong> {airportData.geoname_id}
-      </p>
+          <p><strong>Latitud:</strong> {lat}</p>
+          <p><strong>Longitud:</strong> {lng}</p>
+          <p><strong>ID Geoname:</strong> {airportData.geoname_id}</p>
         </div>
         <div className="absolute inset-0 opacity-20">
           <Image src={flightImage} alt="Fondo" className="details-card-image" />
         </div>
+      </div>
+      <br />
+
+      {/* Mapa de Google */}
+      <div className="map-container">
+        <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}>
+          <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={12}>
+            <Marker position={center} />
+          </GoogleMap>
+        </LoadScript>
       </div>
     </div>
   );
